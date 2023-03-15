de4d074b6c07838bd54bf401f44f7c4178e06802
Gareth <g3vans16@gmail.com>
2023-03-15 10:13:52 +0000

    renamed database and runner application

--- backend/src/main/java/com/codeclan/example/project_test/SoloServiceApplication.java
+++ backend/src/main/java/com/codeclan/example/project_test/SoloServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectTestApplication {
+public class SoloServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectTestApplication.class, args);
+		SpringApplication.run(SoloServiceApplication.class, args);
 	}
 
 }
