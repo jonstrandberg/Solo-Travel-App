205c12666161bf5ba09380bcd70749be63d615d1
Jonathan Strandberg <jonstrandberg7@gmail.com>
2023-03-15 23:35:32 +0000

    two column names for photos

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 import { createStackNavigator } from "@react-navigation/stack";
 import { getLocations } from "../services/LocationService";
@@ -23,15 +23,19 @@ const CitiesList = () => {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
         data={location}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handleCityPress(item)}>
-            <Text>{item.name}, {item.country.name}</Text>
+          <TouchableOpacity
+            onPress={() => handleCityPress(item)}
+            style={styles.item}
+          >
+            <Text style={styles.itemText}>{item.name}, {item.country.name}</Text>
           </TouchableOpacity>
         )}
+        numColumns={2}
       />
     </View>
   );
@@ -45,5 +49,29 @@ const Home = () => {
     </Stack.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+    backgroundColor: '#FFF',
+  },
+  item: {
+    flex: 1,
+    height: 120,
+    margin: 5,
+    padding: 10,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    elevation: 5,
+  },
+  itemText: {
+    fontSize: 16,
+    color: '#254C94',
+    fontWeight: 'bold',
+  },
+});
 
 export default Home;
