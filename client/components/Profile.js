6cab8977c2731a3fd3d81ca9b97ed47fb8356505
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-03-17 10:08:11 +0000

    profile fix

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,9 +1,6 @@
 
 import { useEffect, useState } from "react"
-import { getUserProfile } from "../services/UserService"
-import { Image, FlatList, View, Text, StyleSheet, TextInput, Button, TouchableOpacity, } from "react-native";
-import { createStackNavigator } from '@react-navigation/stack';
-import { useRoute } from '@react-navigation/native';
+import { Image, View, Text, StyleSheet, TextInput, Button } from "react-native";
 import {
     updateUserProfileName,
     updateUserProfileHomeTown,
@@ -15,20 +12,10 @@ import {
 } from "../services/UserService";
 
 
-const backupImage = 'https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg';
+const placeholderImage = 'https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg';
 
 const Profile = () => {
     const [user, setUser] = useState();
-
-    useEffect(() => {
-        getUserProfile(3)
-        .then(data => {
-            console.log(data);
-            setUser(data);
-        })
-        .catch(error => console.log(error))
-    }, []);
-
     const [profile, setProfile] = useState([]);
     const [newName, setNewName] = useState("")
     const [newHomeTown, setNewHomeTown] = useState("")
@@ -45,9 +32,12 @@ const Profile = () => {
     const [isSavingHomeTown, setIsSavingHomeTown] = useState(false);
 
     useEffect(() => {
-        getUserProfile("4").then((json) => {
-            setProfile(json);
-        });
+        getUserProfile(5)
+            .then(data => {
+                console.log(data);
+                setProfile(data);
+            })
+            .catch(error => console.log(error))
     }, []);
 
     const handleUpdateName = async () => {
@@ -104,26 +94,15 @@ const Profile = () => {
     }
 
     return (
-    //NEW
-    <View style={styles.userContainer}>
-            <Image
-            source={{ uri: user?.avatarUrl?user.avatarUrl: backupImage}}
-            style={{ width: 100, height: 100, borderRadius: 50, alignSelf: 'center'}}
-            />
-            <Text style={styles.userProfileName}>User profile name: {user?.displayName?user.displayName: ""}</Text>
-            <Text style={styles.userProfileAge}>Age: {user?.age?user.age: ""}</Text>
-            <Text style={styles.userProfileHometown}>Hometown: {user?.homeTown?user.homeTown: ""}</Text>
-            <Text style={styles.userProfileNationality}>Nationality: {user?.nationality?user.nationality: ""}</Text>
-            <Text style={styles.userProfileLocation}>Location: {user?.location?user.location.name: ""}</Text>
-            
-        </View>
-        
-        //END
-    
+
         <View style={styles.container}>
+            <Image
+                source={{ uri: profile?.avatarUrl ? profile.avatarUrl : placeholderImage }}
+                style={{ width: 100, height: 100, borderRadius: 50, alignSelf: 'center' }}
+            />
             <View style={styles.profileInfo}>
                 <View style={styles.row}>
-            </View>
+                </View>
             </View>
             <View style={styles.profileInfo}>
                 <View style={styles.row}>
@@ -271,6 +250,7 @@ const Profile = () => {
                 </View>
             </View>
         </View>
+
     );
 }
 
@@ -321,27 +301,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         paddingHorizontal: 20,
     },
-    userProfileName: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    userProfileAge: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        
-    },
-    userProfileHometown: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    userProfileNationality: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    userProfileLocation: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    }
+
 });
 
 export default Profile
