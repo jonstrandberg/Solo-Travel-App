0830a2553851b683246a3b72f01880cb2cf40a6e
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-03-20 16:15:35 +0000

    user detail form

--- client/Screens/UserDetailsScreen.js
+++ client/Screens/UserDetailsScreen.js
@@ -1,15 +1,38 @@
 import { useEffect, useState } from "react"
 import { StyleSheet, SafeAreaView, View, Text, TextInput, TouchableOpacity } from "react-native"
 import { useNavigation } from "@react-navigation/native"
-import { auth } from "../firebase";
+import firebase from 'firebase/app'
+import { auth } from "../firebase"
 
 const UserDetailsScreen = () => {
+    const [uid, setUid] = useState('')
     const [name, setName] = useState('')
+    const [avatar, setAvatar] = useState('')
+    const [homeTown, setHomeTown] = useState('')
     const [nationality, setNationality] = useState('')
+    const [age, setAge] = useState('')
+    const [location, setLocation] = useState('')
+    const [interests, setInterests] = useState('')
 
     const navigation = useNavigation()
 
+
+    useEffect(() => {
+        const user = auth.currentUser
+        if (user) {
+            setUid(user.uid)
+        }
+    }, [])
+
     const handleUserDetailCompletion = () => {
+        console.log('UID:', uid)
+        console.log('Name:', name)
+        console.log('Avatar:', avatar)
+        console.log('Home town:', homeTown)
+        console.log('Nationality:', nationality)
+        console.log('Age:', age)
+        console.log('Location:', location)
+        console.log('Interests:', interests)
         navigation.navigate("Navigator")
     }
 
@@ -26,9 +49,39 @@ const UserDetailsScreen = () => {
                     style={styles.input}
                 />
                 <TextInput
+                    placeholder="Profile photo url"
+                    value={avatar}
+                    onChangeText={text => setAvatar(text)}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Home Town"
+                    value={homeTown}
+                    onChangeText={text => setHomeTown(text)}
+                    style={styles.input}
+                />
+                <TextInput
                     placeholder="Nationality"
                     value={nationality}
                     onChangeText={text => setNationality(text)}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Age"
+                    value={age}
+                    onChangeText={text => setAge(text)}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Current Location"
+                    value={location}
+                    onChangeText={text => setLocation(text)}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Interests"
+                    value={interests}
+                    onChangeText={text => setInterests(text)}
                     style={styles.input}
                 />
             </View>
@@ -37,7 +90,7 @@ const UserDetailsScreen = () => {
                     onPress={handleUserDetailCompletion}
                     style={[styles.button, styles.button]}
                 >
-                    <Text>Submit Details</Text>
+                    <Text style={styles.buttonText}>Submit Details</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
