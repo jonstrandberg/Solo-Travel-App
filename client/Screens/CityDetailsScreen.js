470ae15037ff2d52ffe1baa5af7e7222d30e36a1
Jonathan Strandberg <jonstrandberg7@gmail.com>
2023-03-21 01:01:07 +0000

    rollback until working

--- client/Screens/CityDetailsScreen.js
+++ client/Screens/CityDetailsScreen.js
@@ -4,7 +4,6 @@ import { useRoute } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { getEventsByLocationId } from "../services/EventService";
 import AddEventScreen from './AddEventScreen';
-import Header from '../components/Header';
 
 const placeholderCitiyImage = 'https://media.istockphoto.com/photos/alberta-wilderness-near-banff-picture-id583809524?b=1&k=20&m=583809524&s=612x612&w=0&h=ZH0lrJI2ypyxvWQRtpwYcBFZoLLI4XdHWX5xP3JKkKQ='
 
@@ -36,124 +35,70 @@ function CityDetailsScreen({ navigation }) {
   };
   
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerText}>{city.name}, {city.country.name}</Text>
-      </View>
-      <Image
-        source={{ uri: city?.imageUrl ? city.imageUrl : placeholderCitiyImage }}
-        resizeMode="cover"
-        style={styles.imageUrl}
-      />
-      <View style={styles.eventListContainer}>
-    <View style={styles.eventsHeaderContainer}>
-    <Text style={styles.eventsHeaderText}>Events</Text>
-    </View>
-        <FlatList
-          data={event}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => handleEventPress(item)} style={styles.eventContainer}>
-              {/* <Image source={{ uri: item.image }} style={{ width: 80, height: 80 }} /> */}
-              <Text style={styles.eventTitle}>{item.title}</Text>
+    <Stack.Navigator>
+      <Stack.Screen name="Single City Details"
+      options={{ headerShown: false }}>
+        {() => (
+          <View style={styles.container}>
+            <Text style={styles.cityName}>City name: {city.name}</Text>
+            <Text style={styles.country}>Country: {city.country.name}</Text>
+            <Image source={{uri: city?.imageUrl ? city.imageUrl : placeholderCitiyImage}} resizeMode="contain" style={styles.imageUrl}></Image>
+            <Text>Events:</Text>
+            <FlatList
+              data={event}
+              keyExtractor={(item, index) => index.toString()}
+              renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => handleEventPress(item)}>
+                  <Text>{item.title}</Text>
+                </TouchableOpacity>)} />
+            <TouchableOpacity style={styles.button} onPress={handleAddEventPress}>
+              <Text style={styles.buttonText}>Add Event</Text>
             </TouchableOpacity>
-          )}
-        />
-      </View>
-      <TouchableOpacity onPress={handleAddEventPress} style={styles.addButton}>
-        <Text style={styles.addButtonText}>Add Event</Text>
-      </TouchableOpacity>
-    </View>
+          </View>
+        )}
+      </Stack.Screen>
+      <Stack.Screen name="Add Event" component={AddEventScreen} options={{ title: 'Add Event', headerShown: false }} />
+    </Stack.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
-    alignItems: 'center',
-    paddingVertical: 40,
-  },
-  headerContainer: {
-    backgroundColor: '#0B909B',
-    alignItems: 'center',
     justifyContent: 'center',
-    width: '100%',
-    height: 60,
-    top: 0,
-  },
-  headerText: {
-    color: '#FFFFFF',
-    fontSize: 24,
-    fontWeight: 'bold',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
   },
   cityName: {
     fontSize: 24,
     fontWeight: 'bold',
+    marginVertical: 20,
   },
   country: {
-    fontSize: 16,
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  button: {
+    backgroundColor: '#254C94',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
   imageUrl: {
-    width: '100%',
-    height: 200,
-  },
-  eventListContainer: {
-    width: '85%',
-    alignSelf: 'center',
-    paddingBottom: 2,
-    marginBottom: 2,
-    borderRadius: 10,
-    backgroundColor: '#FFFFFF',
-  },
-  eventsHeaderText: {
-    color: '#254C94',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  eventContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: '#254C94',
-    height: 40,
-    width: '100%',
-    borderRadius: 10,
-    marginVertical: 10,
-    flexWrap: 'wrap',
-  },
-  eventTitle: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    width: '100%',
-    height: '100%',
-    lineHeight: 40,
     flex: 1,
-  },
-  eventsHeading: {
-    color: '#254C94',
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    alignSelf: 'center',
-  },
-  addButton: {
-    backgroundColor: '#254C94',
-    paddingVertical: 5, 
-    paddingHorizontal: 10, 
-    borderRadius: 10,
-    alignSelf: 'center', 
-    marginBottom: 20,
-  },
-  addButtonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+    justifyContent: 'center',
+    width:'100%',
+  }
 });
+
 export default CityDetailsScreen;
 
